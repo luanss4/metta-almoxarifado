3428cda205c9fd1a62c762f4ab6fc15ed2700b42
FNXI <luan.s.santana.prestserv@petrobras.com.br>
2025-03-22 19:46:22 -0300

    frontend react

--- metta-almoxarifado/src/App.js
+++ metta-almoxarifado/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ProductList from "./ProductList";
+import Home from "./Home";
+import "./App.css";
+import "./menu.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          {/* Menu de navegação */}
+          <header className="App-header">
+            <nav className="menu">
+              <ul>
+                <li>
+                  <Link to="/">Início</Link>
+                </li>
+                <li>
+                  <Link to="/produtos">Produtos</Link>
+                </li>
+              </ul>
+              {/* Adicionando a logo */}
+              <div className="logo-container">
+                <img
+                    src="/1710100065655.jpg"
+                    alt="Logo"
+                    className="logo"
+                />
+              </div>
+            </nav>
+          </header>
+
+          {/* Configuração das Rotas */}
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/produtos" element={<ProductList />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
   );
 }
 
