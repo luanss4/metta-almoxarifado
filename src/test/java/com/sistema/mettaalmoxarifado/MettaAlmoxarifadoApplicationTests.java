c9498211a19d28243fd5d120ab980eb2afca5b65
FNXI <luan.s.santana.prestserv@petrobras.com.br>
2025-03-22 16:50:11 -0300

    frontend react

---
+++ src/test/java/com/sistema/mettaalmoxarifado/MettaAlmoxarifadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.mettaalmoxarifado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MettaAlmoxarifadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
